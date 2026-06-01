5c9bd00d4f1e77e703fa44def58ef87328c396d9
Aslan Carlos <aslancarlos@gmail.com>
2026-06-01 08:58:24 -0300

    Revert "feat(ui): apply real IDIRA layout to homepage (hero + promise band)"
    This reverts commit 98b33c0a0a473849b73c70c4fac1b33f2846a492.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import Hero from '../components/Hero'
 import ProblemSection from '../components/ProblemSection'
 import ArchitectureDiagram from '../components/ArchitectureDiagram'
-import IdiraPromise from '../components/IdiraPromise'
 
 export default function HomePage() {
   return (
@@ -9,7 +8,6 @@ export default function HomePage() {
       <Hero />
       <ProblemSection />
       <ArchitectureDiagram />
-      <IdiraPromise />
     </>
   )
 }
